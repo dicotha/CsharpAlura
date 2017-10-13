54b77ab94febbedce608f5a7b46e526d036484c7
ESKENAZI\waldir.bertuqui <wbn_k@hotmail.com>
2017-10-13 16:23:29 -0300

    inicio de cx eletronico

---
+++ AluraC/ContaPoupanca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraC
+{
+    class ContaPoupanca: Conta
+    {
+        public override bool Saca(double valor)
+        {
+            this.Saldo -= (valor + 0.10);
+            return true;
+        }
+    }
+}
